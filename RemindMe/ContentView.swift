fc2fdce2668f37f147c3ef8db9abe08629f888e2
tanmayxx12 <troy12rollsza@gmail.com>
2025-02-24 12:12:55 +0530

    Initial Commit

---
+++ RemindMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemindMe
+//
+//  Created by Tanmay . on 24/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Tanmay . on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
